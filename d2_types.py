7718f842ef5f7e90361ca2c808f6ac18a2141ad3
JR-09-lag <joelramodominguez17@gmail.com>
2025-09-15 21:50:15 -0600

    d2: Crear variables y funcion que imprime tipos

---
+++ d2_types.py
@@ -0,0 +1,14 @@
+numero = 42
+decimal = 3.14
+texto = 'Hola mundo'
+verdadero = True
+lista = [1,2,3,4,5]
+
+def print_types():
+    print("numero", type(numero))
+    print("decimal", type(decimal))
+    print("Texto", type(texto))
+    print("booleano", type(verdadero))
+    print("Lista", type(lista))
+
+print_types()
